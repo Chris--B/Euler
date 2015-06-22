b83d052ed56ca8cdbbabeb6a3dd82ae2c434a629
Chris Butler <chrisbutler296@gmail.com>
2015-06-22 06:39:06 -0600

    Added problem discovery
    `Euler.main()` will now find existing problem files (of a specific
    format) and run them.
    
    Initial work on the infrastructure to load arbitrary lists of problems
    is done, we just need a runner now.
    
    Added some hefty comments, and inadvertently fixed a bug in
    `format_time` which would have caused times < 1 nanosecond to return
    None.

--- Euler/__init__.py
+++ Euler/__init__.py
@@ -1,38 +1,75 @@
+import importlib
 import time
-
-import Euler.euler001
-import Euler.euler002
-import Euler.euler003
-import Euler.euler004
 
 def main():
 	times = []
 
-	for (i, problem) in enumerate([euler001, euler002, euler003, euler004], start=1):
+	for (i, problem) in solved_problems():
 		start = time.clock()
 		err = problem.check_given()
 		end = time.clock()
 		times.append(end - start)
+
 		# Should these be exceptions?
 		if err:
 			print(err)
 		else:
-			print(
-				"#{:0>3}: {:<10} ({})".format(i, problem.compute_answer(), format_time(times[-1])))
+			print("#{:0>3}: {:<10} ({})"
+			      .format(i, problem.compute_answer(), format_time(times[-1])))
 
 	print()
-	print("Total", format_time(sum(times)))
-	print("Ave  ", format_time(sum(times) / i))
+	print("Problems", format_time(sum(times)))
+
+
+def solved_problems(problems=None):
+	"""
+	Import modules of the form Euler.euler###, where ### is a three digit number with zero padding.
+	If it cannot find the module, it will silently skip it.
+
+	Return a generator of tuples - the problem number and the module object.
+
+	If `problems` is not None, then it is iterated over and only those problems are imported
+	and yielded. Otherwise, it is as if range(1, 1000) was passed.
+
+	For example, `list(solved_problems()) == [(2, <module object>), (27, <module object>)]`
+	when only problems 2 and 27 have been solved.
+
+	Or, `list(solved_problems([2, 27])) == [(2, <module object>), (27, <module object>)]`
+	if at least problems 2 and 27 have been solved.
+	"""
+
+	if problems is None:
+		# It will be a long while before Project Euler breaks 1000 problems.
+		# e.g.:
+		#   Project Euler launched in 2001,
+		#   Problem 522 is due to be published on June 27th, 2015
+		problems = range(1, 1000)
+
+	for problem in problems:
+		problem_str = "Euler.euler{:03d}".format(problem)
+		try:
+			yield (problem, importlib.import_module(problem_str))
+		# We couldn't find the module, but maybe we just skipped this one? Keep going.
+		except ImportError as e:
+			pass
 
 def format_time(time):
+	"""
+	Return a string "### unit", where ### is a space-padded 3-digit number (usually) and "unit" is one of:
+	s, ms, us, ns, representing seconds, milliseconds, microseconds, and nanoseconds.
+
+	If time is > 1, "s" is used and padding may be overwritten if time >= 1000.
+	If time > 1e-3, then it is multiplied by 1e3 and rounded to the nearest integer.
+	If time > 1e-6, likewise.
+	Otherwise, it is multiplied by 1e9 and rounded. Sub-nanosecond labels don't make sense for our
+	application (timing Python code), so they are not supported.
+	"""
+
 	if time > 1:
 		return "{:>3} s".format(int(time + 0.5))
 	elif time > 1e-3:
 		return "{:>3} ms".format(int(time * 1e3 + 0.5))
 	elif time > 1e-6:
 		return "{:>3} us".format(int(time * 1e6 + 0.5))
-	# This kind of resolution in a language like Python is hardly meaningful.
-	elif time > 1e-8:
-		return "{:>3} ns".format(int(time * 1e9 + 0.5))
 	else:
-		return "Barely any time at all."
+		return "{:>3} ns".format(int(time * 1e9 + 0.5))
