023c8825112e587a977aa76cbbccd95d3dc4b15b
Chris Butler <chrisbutler296@gmail.com>
2015-07-10 23:50:38 -0600

    Updated the template for new problems.

--- Euler/template.py
+++ Euler/template.py
@@ -2,12 +2,10 @@
 problem_text = """
 """
 
+
+
 def compute_answer():
 	pass
 
-def check_given():
-	expected = 0
-	actual = None
-	if actual != expected:
-		return "#@@@: Found {}, but expected {}".format(actual, expected)
-	return None
+given = [
+]
